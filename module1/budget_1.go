d1d47523dc6368e27ff10f0730a7425ee1755bb8
Yuri Campolongo <yuricampolongo@outlook.com>
2021-04-28 21:45:46 -0400

    - module 1

--- module1/budget_1.go
+++ module1/budget_1.go
@@ -1,5 +1,11 @@
 package module1
 
-// Budget stores budget information
+type Budget struct {
+	Max   float32
+	Items []Item
+}
 
-// Item stores item information
+type Item struct {
+	Description string
+	Price       float32
+}
